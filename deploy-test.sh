13022808b9158018014d5953f21a3c4395aa8f12
Sreeprakash Neelakantan <sree@schogini.com>
2020-07-27 19:26:17 +0000

    Sree

--- deploy-test.sh
+++ deploy-test.sh
@@ -2,7 +2,7 @@
 
 docker network create tmp-net > log.txt 2>&1
 docker container rm -f tmp-web >> log.txt 2>&1
-docker run --rm -d --name tmp-web -p 443:8123  --net tmp-net -e WEB=$1 $2 >> log.txt 2>&1
+docker run --rm -d --name tmp-web --net tmp-net -e WEB=$1 $2 >> log.txt 2>&1
 
 # docker ps
 sleep 20
